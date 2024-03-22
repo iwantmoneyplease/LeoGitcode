9c16d3728d612edc249a846e0bc3b63281180d80
Leo Millvik <leo.millvik@learnet.se>
2024-03-22 09:05:21 +0100

    first commit: addede project

---
+++ grej/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace grej
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int run = 0;
+            while(true)
+            {
+                Console.WriteLine("Välj i menyn (1 eller 2)");
+                run = int.Parse(Console.ReadLine());
+                if (run == 1)
+                {
+                    Greetings();
+                }
+                else if (run == 2)
+                {
+                    Leo();
+                }
+                else
+                {
+                    Console.WriteLine("Hej då");
+                    break;
+                }
+            }
+
+            static void Greetings()
+            {
+                Console.WriteLine("Hej från Gitcode");
+            }
+
+            static void Leo()
+            {
+                Console.WriteLine("Tjenare kexet, sitter du där o smular -Leo");
+            }
+        }
+    }
+}
